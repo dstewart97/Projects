0a31da85f1bc19998bfe4bfa77c7911ab6b9273b
David Stewart <djstewart97@gmail.com>
2024-10-23 22:25:56 -0700

    templates finished, added styling

--- Python/Django/BlogTutorial/django_project/blog/views.py
+++ Python/Django/BlogTutorial/django_project/blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 
 
+posts = [
+    {
+        'author' : 'CoreMS',
+        'title' : 'Blog Post 1', 
+        'content' : 'First post content',
+        'date_posted' : 'August 27, 2018'
+    },
+    {
+        'author' : 'Jane Doe',
+        'title' : 'Blog Post 2', 
+        'content' : 'Second post content',
+        'date_posted' : 'August 28, 2018'
+    }
+]
 
 def home(request):
-    return render(request, 'blog/home.html', context = {})
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context = context)
 
 def about(request):
-    return render(request, 'blog/about.html', context = {})
+    return render(request, 'blog/about.html', {'title': 'About'})
