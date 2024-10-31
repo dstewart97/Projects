871f38c4ae8e9a471d77cd4b5d6f9cf99c077f8b
David Stewart <David.Stewart@creditone.com>
2024-10-31 14:16:01 -0700

    ETL essentially complete; data being passed from model to view successfully

--- Python/Django/Magellan/reviews/services/data_aggregator.py
+++ Python/Django/Magellan/reviews/services/data_aggregator.py
@@ -2,9 +2,12 @@ import pandas as pd
 from reviews.models import ReviewData
 
 
-def get_sentiment_distribution():
+def get_review_count_by_date():
     # Fetch data
     queryset = ReviewData.objects.all().values() 
     df = pd.DataFrame(list(queryset))
-    sentiment_counts = df['sentiment_category'].value_counts().to_dict
-    return sentiment_counts
+    review_count_by_date_df = df.groupby('date').size().reset_index(name = 'total_reviews')
+    review_count_by_date_df = review_count_by_date_df.sort_values('date')
+    review_count_by_date_df['date'] = review_count_by_date_df['date'].dt.strftime('%Y-%m-%d')
+    final_review_count_data = review_count_by_date_df.set_index('date')['total_reviews'].to_dict()
+    return final_review_count_data
--- Python/Django/Magellan/reviews/views.py
+++ Python/Django/Magellan/reviews/views.py
@@ -1,8 +1,22 @@
 #### Django-Specific / Default Packages ####
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .services.data_aggregator import get_review_count_by_date
+import json
 
 
+
+
+# def index(request):
+#     return render(request, 'reviews/index.html', {'title': 'Dashboard'} )
 
 def index(request):
-    return render(request, 'reviews/index.html', {'title': 'Dashboard'} )
+    total_review_count_data = get_review_count_by_date()
+
+    context = {
+        'title': 'Dashboard',
+        'total_review_count_data' : json.dumps(total_review_count_data)
+    }
+
+    return render(request, 'reviews/index.html', context)
+
